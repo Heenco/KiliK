8de2b3d0cae55be7f454c1ab6b8960c6fccb48c4
Manni Kheradmandi <kheradmandi.m@gmail.com>
2025-08-07 21:33:35 +1000

    update

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Header />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup>
+import Header from '~/components/ui/Header.vue'
+</script>
+
+<style>
+/* Global styles can go here */
+</style>
